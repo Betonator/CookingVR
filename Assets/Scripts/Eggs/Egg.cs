5bd16542cea3a0175216b2b6d7aa47795a5bf6d6
Michał <empik123@gmail.com>
2019-11-14 12:53:04 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Eggs/Egg.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour
+{
+    public GameObject eggRaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Skillet")
+        {
+            GameObject Egg = Instantiate(eggRaw) as GameObject;
+            Egg.transform.parent = collision.transform;
+            Egg.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+            Destroy(this.gameObject);
+        }
+    }
+}
